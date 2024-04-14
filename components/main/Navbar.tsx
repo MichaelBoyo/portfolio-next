cd735ff8551179c2ba4a316b33671cc9059bd206
Michael Boyo <boyomichaelbidemi@gmail.com>
2024-04-15 00:05:54 +0100

    mquery

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 md:px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto md:px-[10px]">
         <a
           href="#about-me"
           className="h-auto w-auto flex flex-row items-center"
@@ -23,21 +23,19 @@ const Navbar = () => {
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
-              About me
-            </a>
-            <a href="#skills" className="cursor-pointer">
-              Skills
-            </a>
-            <a href="#projects" className="cursor-pointer">
-              Projects
-            </a>
-          </div>
+        <div className="flex items-center justify-between gap-4   h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+          <a href="#about-me" className="cursor-pointer">
+            About me
+          </a>
+          <a href="#skills" className="cursor-pointer">
+            Skills
+          </a>
+          <a href="#projects" className="cursor-pointer">
+            Projects
+          </a>
         </div>
 
-        <div className="flex flex-row gap-5">
+        <div className="hidden  md:flex flex-row gap-5">
           {Socials.map((social) => (
             <Image
               src={social.src}
